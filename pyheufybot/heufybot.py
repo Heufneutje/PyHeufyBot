025f7b5c40d2069ff0755af6015b88f087e34b6b
Heufneutje <stefan.frijters@hotmail.com>
2014-04-23 23:36:34 +0200

    Don't use Twisted logging

--- pyheufybot/heufybot.py
+++ pyheufybot/heufybot.py
@@ -1,8 +1,5 @@
 from twisted.words.protocols import irc
 from twisted.internet import reactor, protocol
-from twisted.python import log
-from sys import stdout
-import os
 
 class HeufyBot(irc.IRCClient):
     nickname = "PyHeufyBot"
@@ -13,27 +10,24 @@ class HeufyBotFactory(protocol.ReconnectingClientFactory):
     protocol = HeufyBot
 
     def startedConnecting(self, connector):
-        log.msg("*** Connecting to server...")
+        print "*** Connecting to server..."
 
     def buildProtocol(self, addr):
-        log.msg("*** Connected.")
-        log.msg("*** Resetting reconnection delay...")
+        print "*** Connected."
+        print "*** Resetting reconnection delay..."
         self.resetDelay()
         return HeufyBot()
 
     def clientConnectionLost(self, connector, reason):
-        log.msg("*** Connection lost. (Reason: {}".format(reason))
+        print "*** Connection lost. (Reason: {}".format(reason)
         protocol.ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
 
     def clientConnectionFailed(self, connector, reason):
-        log.msg("*** Connection failed. (Reason: {}".format(reason))
-        protocol.ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
+       print "*** Connection failed. (Reason: {}".format(reason)
+       protocol.ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
 
 if __name__ == '__main__':
     heufybot = HeufyBotFactory()
-
-    log.startLogging(stdout)
-    log.addObserver(log.FileLogObserver(open(os.path.join("logs/debug.log"), "a")).emit)
 
     reactor.connectTCP("localhost", 6667, heufybot)
     reactor.run()
