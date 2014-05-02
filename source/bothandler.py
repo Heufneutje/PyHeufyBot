2c3d7c8044627fcaa79b232847724b6fe3c6a962
Heufneutje <stefan.frijters@hotmail.com>
2014-05-02 12:50:13 +0000

    Connect with global config if no server configs are found

--- source/bothandler.py
+++ source/bothandler.py
@@ -21,12 +21,13 @@ class BotHandler(object):
         configList = self.getConfigList()
         if len(configList) == 0:
             print "*** WARNING: No server configs found. Using the global config instead."
+            self.startFactory(globalConfig)
         else:
             for filename in configList:
                 config = Config(filename, self.globalConfig.settings)
                 config.loadConfig()
                 self.startFactory(config)
-            reactor.run()
+        reactor.run()
 
     def startFactory(self, config):
         server = config.getSettingWithDefault("server", "irc.foo.bar")
