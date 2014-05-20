4f48fa8636000a1b780c962288bb588b2760465f
Heufneutje <stefan.frijters@hotmail.com>
2014-05-20 13:35:02 +0000

    Improve error logging in file IO

--- pyheufybot/utils/fileutils.py
+++ pyheufybot/utils/fileutils.py
@@ -1,11 +1,12 @@
-import codecs, os
+import codecs, os, time
 
 def readFile(filePath):
     try:
         with open(filePath, "r") as f:
             return f.read()
     except Exception as e:
-        print "*** ERROR: An exception occurred while reading file \"{}\" ({})".format(filePath, e)
+        today = time.strftime("[%H:%M:%S]")
+        print "{} *** ERROR: An exception occurred while reading file \"{}\" ({})".format(today, filePath, e)
         return None
 
 def writeFile(filePath, line, append=False):
@@ -15,7 +16,9 @@ def writeFile(filePath, line, append=False):
             f.write(line)
             return True
     except Exception as e:
-        print "*** ERROR: An exception occurred while writing file \"{}\" ({})".format(filePath, e)
+        today = time.strftime("[%H:%M:%S]")
+        print "{} *** ERROR: An exception occurred while writing file \"{}\" ({})".format(today, filePath, e)
+        return False
 
 def createDirs(path):
     if not os.path.exists(path):
