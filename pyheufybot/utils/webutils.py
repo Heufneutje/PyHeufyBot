5cf4c61668bafc45e91ac41b2bfacf814ef4507d
Heufneutje <stefan.frijters@hotmail.com>
2014-05-21 09:32:18 +0000

    Make headers a tupel list and restrict response types

--- pyheufybot/utils/webutils.py
+++ pyheufybot/utils/webutils.py
@@ -1,24 +1,35 @@
-import time
+import re, time
 from urllib import urlencode
 from urllib2 import build_opener, Request, urlopen, URLError
 from urlparse import urlparse
 
 class URLResponse(object):
-    def __init__(self, body, domain):
+    def __init__(self, body, domain, responseHeaders):
         self.body = body
         self.domain = domain
+        self.responseHeaders = responseHeaders
 
 def fetchURL(url, extraHeaders=None):
-    headers = [{ "User-agent" , "Mozilla/5.0" }]
+    headers = [( "User-agent", "Mozilla/5.0" )]
     if extraHeaders:
         for header in extraHeaders:
             # For whatever reason headers are defined in different way in opener than they are in a normal urlopen
-            headers.append({ header, extraHeaders[header] })
+            headers.append((header, extraHeaders[header]))
     try:
         opener = build_opener()
         opener.addheaders = headers
         response = opener.open(url)
-        return URLResponse(response.read(), urlparse(response.geturl()).hostname)
+        responseHeaders = response.info().dict
+        print responseHeaders
+        pageType = responseHeaders["content-type"]
+
+        # Make sure we don't download any unwanted things
+        if re.match("^(text/.*|application/((rss|atom|rdf)\+)?xml(;.*)?|application/(.*)json(;.*)?)$", pageType):
+            urlResponse = URLResponse(response.read(), urlparse(response.geturl()).hostname, responseHeaders)
+            response.close()
+            return urlResponse
+        else:
+            response.close()
 
     except URLError as e:
         today = time.strftime("[%H:%M:%S]")
@@ -34,11 +45,22 @@ def postURL(url, values, extraHeaders=None):
     if extraHeaders:
         for header in extraHeaders:
             headers[header] = extraHeaders[header]
+
     data = urlencode(values)
+
     try:
         request = Request(url, data, headers)
         response = urlopen(request)
-        return URLResponse(response.read(), urlparse(response.geturl()).hostname)
+        responseHeaders = response.info().dict
+        pageType = responseHeaders["content-type"]
+
+        # Make sure we don't download any unwanted things
+        if re.match('^(text/.*|application/((rss|atom|rdf)\+)?xml(;.*)?|application/(.*)json(;.*)?)$', pageType):
+            urlResponse = URLResponse(response.read(), urlparse(response.geturl()).hostname, responseHeaders)
+            response.close()
+            return urlResponse
+        else:
+            response.close()
 
     except URLError as e:
         today = time.strftime("[%H:%M:%S]")
@@ -48,3 +70,5 @@ def postURL(url, values, extraHeaders=None):
         elif hasattr(e, "code"):
             reason = "The server couldn't fulfill the request, code: {}".format(e.code)
         print "{} *** ERROR: Post to \"{} \" failed: {}".format(today, url, reason)
+
+print fetchURL("http://heufneutje.net", { "Cache-Control" : "no-cache" }).body
