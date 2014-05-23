32b7aa63eb01b3da90f43ee499a165f52befed1a
Heufneutje <stefan.frijters@hotmail.com>
2014-05-23 09:30:58 +0000

    Remove debug lines and reset Ignore's priority to normal

--- pyheufybot/modules/ignore.py
+++ pyheufybot/modules/ignore.py
@@ -8,7 +8,7 @@ class ModuleSpawner(Module):
         self.name = "Ignore"
         self.trigger = "ignore|unignore"
         self.moduleType = ModuleType.COMMAND
-        self.modulePriority = ModulePriority.ABOVENORMAL
+        self.modulePriority = ModulePriority.NORMAL
         self.messageTypes = ["PRIVMSG"]
         self.helpText = "Usage: ignore (<user>), unignore <user>  | Adds the given user to the bot's ignore list. The format is nick!user@host."
 
@@ -16,8 +16,6 @@ class ModuleSpawner(Module):
         self.ignoreList = []
 
     def execute(self, message):
-        print self.ignorePath
-        print self.ignoreList
         if message.params[0].lower() == "ignore":
             if len(message.params) == 1:
                 if len(self.ignoreList) > 0:
@@ -44,9 +42,7 @@ class ModuleSpawner(Module):
         self.loadData()
 
     def loadData(self):
-        print fileutils.readFile(self.ignorePath)
         if os.path.exists(self.ignorePath):
-            print fileutils.readFile(self.ignorePath)
             self.ignoreList = fileutils.readFile(self.ignorePath).split("\n")
 
     def writeData(self):
