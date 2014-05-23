dae2b56fcd0b7434aa48927c88ebde62527bd965
Heufneutje <stefan.frijters@hotmail.com>
2014-05-23 08:58:01 +0000

    Fix all kinds of bugs in Ignore

--- pyheufybot/modules/ignore.py
+++ pyheufybot/modules/ignore.py
@@ -8,7 +8,7 @@ class ModuleSpawner(Module):
         self.name = "Ignore"
         self.trigger = "ignore|unignore"
         self.moduleType = ModuleType.COMMAND
-        self.modulePriotity = ModulePriority.ABOVENORMAL
+        self.modulePriority = ModulePriority.ABOVENORMAL
         self.messageTypes = ["PRIVMSG"]
         self.helpText = "Usage: ignore (<user>), unignore <user>  | Adds the given user to the bot's ignore list. The format is nick!user@host."
 
@@ -16,23 +16,29 @@ class ModuleSpawner(Module):
         self.ignoreList = []
 
     def execute(self, message):
+        print self.ignorePath
+        print self.ignoreList
         if message.params[0].lower() == "ignore":
             if len(message.params) == 1:
                 if len(self.ignoreList) > 0:
-                    self.bot.msg("Currently ignoring users: {}.".format(", ".join(self.ignoreList)
+                    self.bot.msg(message.replyTo, "Currently ignoring users: {}.".format(", ".join(self.ignoreList)))
                 else:
-                    self.bot.msg("Currently not ignoring any users.")
+                    self.bot.msg(message.replyTo, "Currently not ignoring any users.")
             else:
-                ignore = message.params[1:].lower()
-                self.ignoreList.append(ignore)
-                self.bot.msg("\"{}\" was added to the ignore list.".format(ignore)
+                ignore = " ".join(message.params[1:]).lower()
+                if ignore not in self.ignoreList:
+                    self.ignoreList.append(ignore)
+                    self.writeData()
+                    self.bot.msg(message.replyTo, "\"{}\" was added to the ignore list.".format(ignore))
+                else:
+                    self.bot.msg(message.replyTo, "\"{}\" is already on the ignore list.")
         return True
 
     def onModuleLoaded(self):
         self.loadData()
 
     def onModuleUnloaded(self):
-        pass
+        self.writeData()
 
     def reloadData(self):
         self.loadData()
@@ -42,5 +48,5 @@ class ModuleSpawner(Module):
             self.ignoreList = fileutils.readFile(self.ignorePath).split("\n")
 
     def writeData(self):
-        for ignore in ignoreList:
-            fileutils.writeFile(self.ignorePath, "{}\n".format(ignore), True)
+        for ignore in self.ignoreList:
+            fileutils.writeFile(self.ignorePath, "{}\n".format(ignore))
