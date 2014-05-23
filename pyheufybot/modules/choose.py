241fc6501725c5cc27d42fa5eb3ff1f3c91b0861
Heufneutje <stefan.frijters@hotmail.com>
2014-05-24 00:07:33 +0200

    Implement the Choose module

---
+++ pyheufybot/modules/choose.py
@@ -0,0 +1,27 @@
+import random
+from pyheufybot.moduleinterface import Module, ModuleAccessLevel, ModulePriority, ModuleType
+
+class ModuleSpawner(Module):
+    def __init__(self, bot):
+        super(ModuleSpawner, self).__init__(bot)
+
+        self.name = "Choose"
+        self.trigger = "choose|choice"
+        self.moduleType = ModuleType.COMMAND
+        self.modulePriority = ModulePriority.NORMAL
+        self.accessLevel = ModuleAccessLevel.ANYONE
+        self.messageTypes = ["PRIVMSG"]
+        self.helpText = "Usage: choose <option1>, <option2>, ...  | Makes the bot choose one of the given options."
+
+    def execute(self, message):
+        if len(message.params) == 1:
+            self.bot.msg(message.replyTo, "What do you want me to choose from?")
+        else:
+            params = " ".join(message.params[1:])
+            if ',' in params:
+                options = params.split(',')
+            else:
+                options = params.split()
+            self.bot.msg(message.replyTo, "Choice: {}".format(random.choice(options).strip()))
+
+        return True
