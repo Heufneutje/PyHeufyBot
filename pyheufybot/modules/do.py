80c6cca18f3ddd75dec8375612b426cf01534a10
Heufneutje <stefan.frijters@hotmail.com>
2014-05-31 15:02:34 +0200

    Add remote Do command

--- pyheufybot/modules/do.py
+++ pyheufybot/modules/do.py
@@ -5,16 +5,22 @@ class ModuleSpawner(Module):
         super(ModuleSpawner, self).__init__(bot)
 
         self.name = "Do"
-        self.trigger = "do"
+        self.trigger = "do|doremote"
         self.moduleType = ModuleType.COMMAND
         self.modulePriority = ModulePriority.NORMAL
         self.accessLevel = ModuleAccessLevel.ANYONE
         self.messageTypes = ["PRIVMSG"]
-        self.helpText = "Usage: do <message> | Makes the bot perform the given action."
+        self.helpText = "Usage: do <message>/doremote <target> <message>  | Makes the bot perform the given line."
 
     def execute(self, message):
-        if len(message.params) == 1:
-            self.bot.msg(message.replyTo, "Do what?")
-        else:
-            self.bot.describe(message.replyTo, " ".join(message.params[1:]))
+        if message.params[0].lower() == "do":
+            if len(message.params) == 1:
+                self.bot.msg(message.replyTo, "Do what?")
+            else:
+                self.bot.describe(message.replyTo, " ".join(message.params[1:]))
+        elif message.params[0].lower() == "doremote":
+            if len(message.params) < 3:
+                self.bot.msg(message.replyTo, "Do what?")
+            else:
+                self.bot.describe(message.params[1], " ".join(message.params[2:]))
         return True
