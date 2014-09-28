b3b65e717c092bdabdcb5d13405c681f9e1dfb12
Heufneutje <stefan.frijters@gmail.com>
2014-09-28 22:10:17 +0200

    Add basic client details

--- heufybot/connection.py
+++ heufybot/connection.py
@@ -4,10 +4,13 @@ from twisted.words.protocols import irc
 class HeufyBotConnection(irc.IRC):
     def __init__(self, protocol):
         self.protocol = protocol
+        self.nickname = "PyHeufyBot" #TODO This will be set by a configuration at some point
+        self.ident = "PyHeufyBot" #TODO This will be set by a configuration at some point
+        self.gecos = "PyHeufyBot IRC Bot" #TODO This will be set by a configuration at some point
 
     def connectionMade(self):
-        self.sendLine("NICK PyHeufyBot")
-        self.sendLine("USER PyHeufyBot 0 0 :PyHeufyBot")
+        self.sendMessage("NICK", "PyHeufyBot")
+        self.sendMessage("USER", "PyHeufyBot", "0", "0", ":PyHeufyBot Bot")
         print "OK"
 
     def connectionLost(self, reason=""):
