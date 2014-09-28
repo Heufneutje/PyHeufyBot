b6027aceae21769c2f3dc7baccd5960e83ed9a90
Heufneutje <stefan.frijters@gmail.com>
2014-09-28 22:21:05 +0200

    Add functions for NICK and USER sending. Override sendMessage for debugging.

--- heufybot/connection.py
+++ heufybot/connection.py
@@ -9,12 +9,23 @@ class HeufyBotConnection(irc.IRC):
         self.gecos = "PyHeufyBot IRC Bot" #TODO This will be set by a configuration at some point
 
     def connectionMade(self):
-        self.sendMessage("NICK", "PyHeufyBot")
-        self.sendMessage("USER", "PyHeufyBot", "0", "0", ":PyHeufyBot Bot")
-        print "OK"
+        self.cmdNICK(self.nickname)
+        self.cmdUSER(self.ident, self.gecos)
 
     def connectionLost(self, reason=""):
         print reason
 
     def dataReceived(self, data):
         print data
+
+    def sendMessage(self, command, *parameter_list, **prefix):
+        print command, " ".join(parameter_list)
+        irc.IRC.sendMessage(self, command, *parameter_list, **prefix)
+
+    def cmdNICK(self, nickname):
+        self.sendMessage("NICK", nickname)
+
+    def cmdUSER(self, ident, gecos):
+        # RFC2812 allows usermodes to be set, but this isn't implemented much in IRCds at all.
+        # Pass 0 for usermodes instead.
+        self.sendMessage("USER", ident, "0", "*", ":{}".format(gecos))
