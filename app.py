fe7002839280c518095b0bb327f5ac7c24da8d18
Heufneutje <stefan.frijters@gmail.com>
2014-10-11 20:13:36 +0200

    Move the entry point and add interpreter options

---
+++ app.py
@@ -0,0 +1,30 @@
+from twisted.python import log
+from heufybot.bot import HeufyBot
+from heufybot.utils.logutils import LevelLoggingObserver
+import argparse, logging
+
+
+parser = argparse.ArgumentParser(description="A modular Twisted IRC bot.")
+parser.add_argument("-c", "--config", help="The configuration file to use", type=str, default="heufybot.yaml")
+parser.add_argument("-f", "--logfile", help="The file the log will be written to", type=str, default="heufybot.log")
+parser.add_argument("-l", "--loglevel", help="The logging level the bot will use", type=str, default="INFO")
+options = parser.parse_args()
+
+if __name__ == "__main__":
+    # Determine the logging level
+    numericLevel = getattr(logging, options.loglevel.upper(), None)
+    invalidLogLevel = False
+    if not isinstance(numericLevel, int):
+        numericLevel = logging.INFO
+        invalidLogLevel = True
+
+    # Set up logging
+    observer = LevelLoggingObserver(open(options.logfile, "a"), numericLevel)
+    observer.start()
+
+    # Yell at the user if they specified an invalid log level
+    if invalidLogLevel:
+        log.msg("Picked up invalid log level {}; defaulting to INFO.".format(options.loglevel), logLevel=logging.WARNING)
+
+    # Start the bot
+    heufybot = HeufyBot(options.config)
--- heufybot/bot.py
+++ heufybot/bot.py
@@ -1,17 +1,12 @@
 from twisted.internet import reactor
 from heufybot.config import Config
 from heufybot.factory import HeufyBotFactory
-from heufybot.utils.logutils import LevelLoggingObserver
 
 
 class HeufyBot(object):
-    def __init__(self):
-        self.config = Config("heufybot.yaml")
+    def __init__(self, configFile):
+        self.config = Config(configFile)
         self.config.readConfig()
         self.connectionFactory = HeufyBotFactory(self)
-        observer = LevelLoggingObserver(open("test.log", "a"), 1)
-        observer.start()
         reactor.connectTCP("heufneutje.net", 6667, self.connectionFactory)
         reactor.run()
-
-heufybot = HeufyBot()
