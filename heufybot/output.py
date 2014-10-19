fa66f44cf9783e790a2758b255ad740e712dc667
Heufneutje <stefan.frijters@gmail.com>
2014-10-19 15:57:41 +0200

    Put commands in alphabetical order for my own sanity

--- heufybot/output.py
+++ heufybot/output.py
@@ -5,10 +5,10 @@ class OutputHandler(object):
     def cmdNICK(self, nick):
         self.connection.sendMessage("NICK", nick)
 
+    def cmdQUIT(self, reason):
+        self.connection.sendMessage("QUIT", ":{}".format(reason))
+
     def cmdUSER(self, ident, gecos):
         # RFC2812 allows usermodes to be set, but this isn't implemented much in IRCds at all.
         # Pass 0 for usermodes instead.
         self.connection.sendMessage("USER", ident, "0", "*", ":{}".format(gecos))
-
-    def cmdQUIT(self, reason):
-        self.connection.sendMessage("QUIT", ":{}".format(reason))
