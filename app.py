c837ce0bfa64605ddadcebfc6e60a55ad8261689
Stefan <Heufneutje@users.noreply.github.com>
2014-11-19 10:04:58 +0100

    Move command line argument parsing into the __main__ function

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from heufybot.utils.logutils import LevelLoggingObserver
 import argparse, logging
 
 
-parser = argparse.ArgumentParser(description="A modular Twisted IRC bot.")
-parser.add_argument("-c", "--config", help="The configuration file to use", type=str, default="heufybot.yaml")
-parser.add_argument("-f", "--logfile", help="The file the log will be written to", type=str, default="heufybot.log")
-parser.add_argument("-l", "--loglevel", help="The logging level the bot will use", type=str, default="INFO")
-options = parser.parse_args()
-
 if __name__ == "__main__":
+    # Parse the command line arguments
+    parser = argparse.ArgumentParser(description="A modular Twisted IRC bot.")
+    parser.add_argument("-c", "--config", help="The configuration file to use", type=str, default="heufybot.yaml")
+    parser.add_argument("-f", "--logfile", help="The file the log will be written to", type=str, default="heufybot.log")
+    parser.add_argument("-l", "--loglevel", help="The logging level the bot will use", type=str, default="INFO")
+    options = parser.parse_args()
+    
     # Determine the logging level
     numericLevel = getattr(logging, options.loglevel.upper(), None)
     invalidLogLevel = False
