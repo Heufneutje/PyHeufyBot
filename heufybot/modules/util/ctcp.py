79c42d88b038d6cc17790d5ba1eb2c99ad20aa17
Heufneutje <stefan.frijters@gmail.com>
2014-11-30 11:34:22 +0100

    Implement the CTCP module

---
+++ heufybot/modules/util/ctcp.py
@@ -0,0 +1,49 @@
+from twisted.plugin import IPlugin
+from heufybot import __version__
+from heufybot.channel import IRCChannel
+from heufybot.moduleinterface import BotModule, IBotModule
+from heufybot.utils.timeutils import now
+from zope.interface import implements
+from platform import platform
+
+
+class CTCP(BotModule):
+    implements(IPlugin, IBotModule)
+
+    name = "CTCP"
+
+    def hookBot(self, bot):
+        self.bot = bot
+
+    def actions(self):
+        return [ ("ctcp-message", 1, self.handleCTCP),
+                 ("send-ctcp", 1, self.sendCTCPCommand) ]
+
+    def handleCTCP(self, server, source, message):
+        if not self.bot.moduleHandler.useModuleOnServer(self.name, server):
+            return
+
+        if isinstance(source, IRCChannel):
+            target = source.name
+        else:
+            target = source.nick
+
+        if message.upper().startswith("PING"):
+            self.sendCTCPReply(server, target, "PING", message[5:])
+        elif message.upper().startswith("VERSION"):
+            self.sendCTCPReply(server, target, "VERSION", "PyHeufyBot v{} / {}".format(__version__, platform(())))
+        elif message.upper().startswith("TIME"):
+            self.sendCTCPReply(server, target, "TIME", str(now()))
+        elif message.upper().startswith("SOURCE"):
+            self.sendCTCPReply(server, target, "SOURCE", "https://github.com/Heufneutje/PyHeufyBot/")
+
+    def sendCTCPCommand(self, server, target, ctcpType):
+        self.bot.servers[server].outputHandler.cmdPRIVMSG(target, "\x01{}\x01".format(ctcpType))
+
+    def sendCTCPReply(self, server, target, ctcpType, reply):
+        if reply:
+            self.bot.servers[server].outputHandler.cmdNOTICE(target, "\x01{} {}\x01".format(ctcpType, reply))
+        else:
+            self.bot.servers[server].outputHandler.cmdNOTICE(target, "\x01{}\x01".format(ctcpType))
+
+ctcp = CTCP()
