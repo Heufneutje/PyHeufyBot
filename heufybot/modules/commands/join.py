2c39c0ada6ca42c5960a5c05bd32a60ba413d886
Heufneutje <stefan.frijters@gmail.com>
2014-12-21 12:34:10 +0100

    Implement the Join command

---
+++ heufybot/modules/commands/join.py
@@ -0,0 +1,25 @@
+from twisted.plugin import IPlugin
+from heufybot.moduleinterface import IBotModule
+from heufybot.modules.commandinterface import BotCommand
+from zope.interface import implements
+
+
+class JoinCommand(BotCommand):
+    implements(IPlugin, IBotModule)
+
+    name = "Join"
+
+    def triggers(self):
+        return ["join"]
+
+    def execute(self, server, source, command, params, data):
+        if len(params) < 1:
+            self.bot.servers[server].outputHandler.cmdPRIVMSG(source, "Join what?")
+            return
+
+        if len(params) > 2:
+            self.bot.servers[server].outputHandler.cmdJOIN(params[0], params[1])
+        else:
+            self.bot.servers[server].outputHandler.cmdJOIN(params[0])
+
+joinCommand = JoinCommand()
