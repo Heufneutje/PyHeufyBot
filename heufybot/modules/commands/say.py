f03f671ab7ceb7ea5a56587e054659df8dc89738
Heufneutje <stefan.frijters@hotmail.com>
2014-12-27 16:52:02 +0100

    Implement Do commands in the Say module

--- heufybot/modules/commands/say.py
+++ heufybot/modules/commands/say.py
@@ -10,15 +10,22 @@ class SayCommand(BotCommand):
     name = "Say"
 
     def triggers(self):
-        return ["say", "sayto"]
+        return ["say", "sayto", "do", "doto"]
 
     def execute(self, server, source, command, params, data):
         if command == "say" and len(params) < 1 or command == "sayto" and len(params) < 2:
             self.bot.servers[server].outputHandler.cmdPRIVMSG(source, "Say what?")
             return
+        if command == "do" and len(params) < 1 or command == "doto" and len(params) < 2:
+            self.bot.servers[server].outputHandler.cmdPRIVMSG(source, "Do what?")
+            return
         if command == "say":
             self.bot.servers[server].outputHandler.cmdPRIVMSG(source, " ".join(params))
         elif command == "sayto":
             self.bot.servers[server].outputHandler.cmdPRIVMSG(params[0], " ".join(params[1:]))
+        elif command == "do":
+            self.bot.servers[server].outputHandler.ctcpACTION(source, " ".join(params))
+        elif command == "doto":
+            self.bot.servers[server].outputHandler.ctcpACTION(params[0], " ".join(params[1:]))
 
 sayCommand = SayCommand()
