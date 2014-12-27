770e36d524bbef8790b6e941a07f403c6b3f2ea0
Heufneutje <stefan.frijters@hotmail.com>
2014-12-27 16:50:28 +0100

    Fix action priorities

--- heufybot/modulehandler.py
+++ heufybot/modulehandler.py
@@ -41,12 +41,12 @@ class ModuleHandler(object):
             if action not in self.actions:
                 self.actions[action] = []
             for actionData in actionList:
-                for index, handlerData in enumerate(actions[action]):
+                for index, handlerData in enumerate(self.actions[action]):
                     if handlerData[1] < actionData[1]:
                         self.actions[action].insert(index, actionData)
                         break
-                    else:
-                        self.actions[action].append(actionData)
+                else:
+                    self.actions[action].append(actionData)
 
         # Add the module to the list of loaded modules and call its load hooks
         self.loadedModules[module.name] = module
