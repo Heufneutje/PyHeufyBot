83b0214cccb2850bfe082b2721a4af3c8631559e
HubbeKing <hubbe128@gmail.com>
2015-01-25 10:40:57 +0200

    Do the thing with the numbers
    Indexes are from 0 to 15, int((degrees + 11.25) / 22.5 should always be between
    1 and 16, unless wind figures out how to travel ninth-dimensionally and blow in
    >361 degrees somehow.

--- heufybot/modules/commands/weather.py
+++ heufybot/modules/commands/weather.py
@@ -158,7 +158,6 @@ class WeatherCommand(BotCommand):
     def _convertWindDegToCardinal(self, degrees):
         directions = [ "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW",
                        "NNW" ]
-        i = int((degrees + 11.25) / 22.5)
-        return directions[i % 16]
+        return directions[int((degrees + 11.25) / 22.5)]
 
 weatherCommand = WeatherCommand()
