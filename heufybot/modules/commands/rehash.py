89414aaf9ef83bda59a693ac45e04cc6a8149a39
Heufneutje <stefan.frijters@hotmail.com>
2015-04-26 00:16:17 +0200

    Implement the Rehash command

---
+++ heufybot/modules/commands/rehash.py
@@ -0,0 +1,27 @@
+from twisted.plugin import IPlugin
+from heufybot.config import ConfigError
+from heufybot.moduleinterface import IBotModule
+from heufybot.modules.commandinterface import BotCommand
+from zope.interface import implements
+
+
+class RehashCommand(BotCommand):
+    implements(IPlugin, IBotModule)
+
+    name = "Rehash"
+
+    def load(self):
+        self.help = "Commands: rehash | Rehashes the bot's configuration files."
+        self.commandHelp = {}
+
+    def checkPermissions(self, server, source, user, command):
+        return not self.bot.moduleHandler.runActionUntilFalse("checkadminpermission", server, source, user, "rehash")
+
+    def execute(self, server, source, command, params, data):
+        try:
+            self.bot.config.loadConfig()
+            self.bot.outputHandler.cmdPRIVMSG(source, "Rehashed the configuration successfully.")
+        except ConfigError as e:
+            self.bot.outputHandler.cmdPRIVMSG(source, e)
+
+rehashCommand = RehashCommand()
