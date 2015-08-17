b326a59a5ffae2d96ab7ac9c57bab1e5e237b9f7
Heufneutje <stefan.frijters@gmail.com>
2015-08-17 14:25:48 +0200

    Fix broken CTCP replies in channels

--- heufybot/modules/util/ctcp.py
+++ heufybot/modules/util/ctcp.py
@@ -20,19 +20,14 @@ class CTCP(BotModule):
         if not self.bot.moduleHandler.useModuleOnServer(self.name, server):
             return
 
-        if isinstance(source, IRCChannel):
-            target = source.name
-        else:
-            target = source.nick
-
         if message.upper() == "PING" or message.upper().startswith("PING "):
-            self.sendCTCPReply(server, target, "PING", message[5:])
+            self.sendCTCPReply(server, user.nick, "PING", message[5:])
         elif message.upper() == "VERSION":
-            self.sendCTCPReply(server, target, "VERSION", "PyHeufyBot v{} / {}".format(__version__, platform()))
+            self.sendCTCPReply(server, user.nick, "VERSION", "PyHeufyBot v{} / {}".format(__version__, platform()))
         elif message.upper() == "TIME":
-            self.sendCTCPReply(server, target, "TIME", strftimeWithTimezone(now()))
+            self.sendCTCPReply(server, user.nick, "TIME", strftimeWithTimezone(now()))
         elif message.upper() == "SOURCE":
-            self.sendCTCPReply(server, target, "SOURCE", "https://github.com/Heufneutje/PyHeufyBot/")
+            self.sendCTCPReply(server, user.nick, "SOURCE", "https://github.com/Heufneutje/PyHeufyBot/")
 
     def sendCTCPCommand(self, server, target, ctcpType):
         self.bot.servers[server].outputHandler.cmdPRIVMSG(target, "\x01{}\x01".format(ctcpType))
