8d8679964680215d52b6d5e41566bf62a1877fa9
Heufneutje <stefan.frijters@gmail.com>
2016-01-10 18:08:35 +0100

    Add a module to list all available commands
    Closes #32

---
+++ heufybot/modules/commands/commands.py
@@ -0,0 +1,27 @@
+from twisted.plugin import IPlugin
+from heufybot.moduleinterface import IBotModule
+from heufybot.modules.commandinterface import BotCommand
+from zope.interface import implements
+
+
+class CommandsCommand(BotCommand):
+    implements(IPlugin, IBotModule)
+
+    name = "Commands"
+
+    def triggers(self):
+        return ["commands"]
+
+    def load(self):
+        self.help = "Commands: commands | Lists all bot commands from the modules tht are enabled for this server."
+        self.commandHelp = {}
+
+    def execute(self, server, source, command, params, data):
+        commandsList = []
+        for moduleName, module in self.bot.moduleHandler.loadedModules.iteritems():
+            if self.bot.moduleHandler.useModuleOnServer(moduleName, server) and isinstance(module, BotCommand):
+                commandsList += module.triggers()
+        msg = "Available commands: {}".format(", ".join(sorted(commandsList)))
+        self.bot.servers[server].outputHandler.cmdPRIVMSG(source, msg)
+
+commandsCommand = CommandsCommand()
