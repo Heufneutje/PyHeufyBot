cdf85e75ac195d6d81b9b0ef27b68205f95c9ae5
Heufneutje <stefan.frijters@gmail.com>
2016-05-16 17:15:07 +0200

    Estimated weather conditions don't have a latest update time

--- heufybot/modules/commands/weather.py
+++ heufybot/modules/commands/weather.py
@@ -130,17 +130,22 @@ class WeatherCommand(BotCommand):
         winddir = cond["wind_dir"]
         windspeedMiles = cond["wind_mph"]
         windspeedMs = round(cond["wind_kph"] / 3.6, 1)
-        latestUpdate = (timestamp(now()) - int(cond["observation_epoch"])) / 60
-        latestUpdateStr = "{} minute(s) ago".format(latestUpdate) if latestUpdate > 0 else "just now"
-
         tempDiff = float(tempC) - float(feelslikeC)
         feelslikeStr = ""
         if tempDiff > 3.0 or tempC < -3.0:
             feelslikeStr = "(feels like {}°C / {}°F) ".format(feelslikeC, feelslikeF)
 
+        if len(cond["estimated"]) > 0:
+            estimate = cond["estimated"]["description"]
+            return "Temp: {}°C / {}°F {}| Weather: {} | Humidity: {} | Wind Speed: {} m/s / {} mph | " \
+                   "Wind Direction: {} | {}.".format(tempC, tempF, feelslikeStr, description, humidity, windspeedMs,
+                                                     windspeedMiles, winddir, estimate)
+
+        latestUpdate = (timestamp(now()) - int(cond["observation_epoch"])) / 60
+        latestUpdateStr = "{} minute(s) ago".format(latestUpdate) if latestUpdate > 0 else "just now"
         return "Temp: {}°C / {}°F {}| Weather: {} | Humidity: {} | Wind Speed: {} m/s / {} mph | " \
-               "Wind Direction: {} | Latest Update: {}.".format(tempC, tempF, feelslikeStr, description, humidity,
-                                                                windspeedMs, windspeedMiles, winddir, latestUpdateStr)
+               "Wind Direction: {} | Latest Update: {}".format(tempC, tempF, feelslikeStr, description, humidity,
+                                                               windspeedMs, windspeedMiles, winddir, latestUpdateStr)
 
     def _parseForecast(self, json):
         daysList = json["forecast"]["simpleforecast"]["forecastday"]
